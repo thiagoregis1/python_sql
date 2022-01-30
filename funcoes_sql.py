2b12884d1fe8b36e4c0d9b05d90c688a6e18156f
Thiago Regis <89661413+thiagoregis1@users.noreply.github.com>
2022-01-30 14:11:18 -0300

    Update funcoes_sql.py
    Adicionado comentários para melhor entendimento

--- funcoes_sql.py
+++ funcoes_sql.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+#FUNÇÃO PARA CRIAR UM DATABASE LOCAL
 def create_database():
     mydb = mysql.connector.connect(
         host='localhost',
@@ -18,12 +19,14 @@ mydb = mysql.connector.connect(
 )
 mycursor = mydb.cursor()
 
+#FUNÇÃO PARA CRIAR UMA TABELA NO DATABASE LOCAL CRIADO NA FUNÇÃO ANTERIOR
 def create_table():
     mycursor.execute("CREATE TABLE alunos (Id INT AUTO_INCREMENT PRIMARY KEY, Nome VARCHAR(255), Matrícula VARCHAR(255), Turma VARCHAR(255))")
     print("Tabela criada com sucesso!")
 
 #create_table()
 
+#FUNÇÃO PARA INSERIR OS DADOS NA TABELA
 def insert_data():
     sql = "INSERT INTO alunos (Nome, Matrícula, Turma) VALUES (%s, %s, %s)"
     val = [
@@ -41,6 +44,7 @@ def insert_data():
 
 #insert_data()
 
+#FUNÇÃO PARA VISUALIZAR TODOS OS DADOS DA TABELA
 def view_data():
     sql = "SELECT * FROM alunos"
 
@@ -52,7 +56,7 @@ def view_data():
 
 #view_data()
 
-
+#FUNÇÃO PARA VISUALIZAR OS DADOS DA TABELA INTEIRA COM FILTRO ESPECÍFICO EM UMA DAS COLUNAS
 def view_filter():
     sql = "SELECT * FROM alunos WHERE Turma = 'BCW23' "
     mycursor.execute(sql)
@@ -62,6 +66,8 @@ def view_filter():
 
 #view_filter()
 
+
+#FUNÇÃO PARA VISUALIZAR OS DADOS DA TABELA INTEIRA COM FILTRO DE 'CONTÉM'EM UMA DAS COLUNAS
 def view_filter_like():
     sql = "SELECT * FROM alunos WHERE Nome LIKE '%Lima%' "
     mycursor.execute(sql)
@@ -72,6 +78,8 @@ def view_filter_like():
 
 #view_filter_like()
 
+
+#FUNÇÃO PARA CRIAR UMA ALTERAÇÃO EM UM CAMPO ESPECÍFICO DA TABELA
 def update_where():
     sql = "UPDATE alunos SET Turma = 'BCW25' WHERE Nome ='Carlos Augusto' "
 
@@ -81,6 +89,8 @@ def update_where():
 
 #update_where()
 
+
+#FUNÇÃO PARA DELETAR UMA LINHA FILTANDO POR UM CAMPO ESPECÍFICO DA TABELA
 def del_where():
     sql = "DELETE FROM alunos WHERE Nome = 'José Vinicius'"
     mycursor.execute(sql)
@@ -89,14 +99,16 @@ def del_where():
 
 #del_where()
 
+#FUNÇÃO PARA DELETAR A TABELA POR INTEIRA
 def drop_table():
     sql = "DROP TABLE alunos"
     mycursor.execute(sql)
     mydb.commit()
-    print(mycursor.rowcount,'Linha(s) alterada(s)!')
+    print('Tabela deletada com sucesso!')
 
 #drop_table()
 
+#FUNÇÃO PARA DELETAR O DATABASE INTEIRO
 def drop_database():
     mycursor.execute("DROP DATABASE sistema_escolar_soul_on")
     print("Database deletado com sucesso!")
